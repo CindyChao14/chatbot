425270c54c91624906482958ff8c0e65b77a9d61
CindyChao14 <cindychao@cloudybay.com.tw>
2018-03-28 11:57:10 +0800

    rename app

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from mychatbot.echobot import urls as echobot
+from chatbot.echobot import urls as echobot
 
 urlpatterns = [
     url(r'^echobot/', include(echobot))
