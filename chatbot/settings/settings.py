afa7b0a2eca2667b27dc5fb0816280e0843d0dad
CindyChao14 <cindychao@cloudybay.com.tw>
2018-03-28 10:13:20 +0800

    line20 modify

--- chatbot/settings/settings.py
+++ chatbot/settings/settings.py
@@ -15,7 +15,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-from chatbot.get_env import get_env_variable
+from .get_env import get_env_variable
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
